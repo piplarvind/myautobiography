9da38778a3442b8dcf5aa59dd5e914ceefbd39e8
lukshya <lukshya.madan@gmail.com>
2015-01-18 13:29:36 +0530

    initial commit

--- application/views/front/includes/top-nav.php
+++
@@ -1,7 +0,0 @@
-<div class="navbar navbar-default">
-    <div class="container">
-        <div class="navbar-header">
-            <div class="navbar-brand">Page Header</div>
-        </div>
-     </div>
-</div>
