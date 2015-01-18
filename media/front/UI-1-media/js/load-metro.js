a23e632ca40fbdc2a07b4bf917a558cfe1a9c2b5
lukshya <lukshya.madan@gmail.com>
2015-01-18 13:16:06 +0530

    initial commit

--- media/front/UI-1-media/js/load-metro.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    if ((document.location.host.indexOf('.dev') > -1) || (document.location.host.indexOf('modernui') > -1) ) {
-        $("<script/>").attr('src', 'UI-1-media/js/metro/metro-loader.js').appendTo($('head'));
-    } else {
-        $("<script/>").attr('src', 'media/front/UI-1-media/js/metro.min.js').appendTo($('head'));
-    }
-})
